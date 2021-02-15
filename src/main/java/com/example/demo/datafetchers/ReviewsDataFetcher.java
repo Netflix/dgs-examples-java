ecc11fd6a898ea81f03af23acafe7f3854cffb6d
Paul Bakker <pbakker@netflix.com>
2021-02-15 10:43:50 -0800

    Example of using local context

--- src/main/java/com/example/demo/datafetchers/ReviewsDataFetcher.java
+++ src/main/java/com/example/demo/datafetchers/ReviewsDataFetcher.java
@@ -14,6 +14,7 @@ import org.dataloader.DataLoader;
 import org.reactivestreams.Publisher;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
@@ -35,15 +36,12 @@ public class ReviewsDataFetcher {
      * For this to work correctly, the datafetcher needs to return a CompletableFuture.
      */
     @DgsData(parentType = DgsConstants.SHOW.TYPE_NAME, field = DgsConstants.SHOW.Reviews)
-    public CompletableFuture<List<Review>> reviews(DgsDataFetchingEnvironment dfe) {
-        //Instead of loading a DataLoader by name, we can use the DgsDataFetchingEnvironment and pass in the DataLoader classname.
-        DataLoader<Integer, List<Review>> reviewsDataLoader = dfe.getDataLoader(ReviewsDataLoader.class);
-
-        //Because the reviews field is on Show, the getSource() method will return the Show instance.
+    public List<Review> reviews(DgsDataFetchingEnvironment dfe) {
         Show show = dfe.getSource();
 
-        //Load the reviews from the DataLoader. This call is async and will be batched by the DataLoader mechanism.
-        return reviewsDataLoader.load(show.getId());
+        //Load the reviews from the pre-loaded localContext.
+        Map<Integer, List<Review>> reviewsForShows = dfe.getLocalContext();
+        return reviewsForShows.get(show.getId());
     }
 
     @DgsData(parentType = DgsConstants.MUTATION.TYPE_NAME, field = DgsConstants.MUTATION.AddReview)
--- src/main/java/com/example/demo/datafetchers/ShowsDatafetcher.java
+++ src/main/java/com/example/demo/datafetchers/ShowsDatafetcher.java
@@ -1,21 +1,27 @@
 package com.example.demo.datafetchers;
 
 import com.example.demo.generated.DgsConstants;
+import com.example.demo.generated.types.Review;
 import com.example.demo.generated.types.Show;
+import com.example.demo.services.DefaultReviewsService;
 import com.example.demo.services.ShowsService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
-import com.netflix.graphql.dgs.InputArgument;
+import graphql.execution.DataFetcherResult;
+import graphql.schema.DataFetchingEnvironment;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @DgsComponent
 public class ShowsDatafetcher {
     private final ShowsService showsService;
+    private final DefaultReviewsService reviewsService;
 
-    public ShowsDatafetcher(ShowsService showsService) {
+    public ShowsDatafetcher(ShowsService showsService, DefaultReviewsService reviewsService) {
         this.showsService = showsService;
+        this.reviewsService = reviewsService;
     }
 
     /**
@@ -23,11 +29,19 @@ public class ShowsDatafetcher {
      * It uses an @InputArgument to get the titleFilter from the Query if one is defined.
      */
     @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.Shows)
-    public List<Show> shows(@InputArgument("titleFilter") String titleFilter) {
-        if (titleFilter == null) {
-            return showsService.shows();
-        }
+    public DataFetcherResult<List<Show>> shows(DataFetchingEnvironment dfe) {
 
-        return showsService.shows().stream().filter(s -> s.getTitle().contains(titleFilter)).collect(Collectors.toList());
+        List<Show> shows = showsService.shows();
+        if (dfe.getSelectionSet().contains("reviews")) {
+
+            Map<Integer, List<Review>> reviewsForShows = reviewsService.reviewsForShows(shows.stream().map(Show::getId).collect(Collectors.toList()));
+
+            return DataFetcherResult.<List<Show>>newResult()
+                    .data(shows)
+                    .localContext(reviewsForShows)
+                    .build();
+        } else {
+            return DataFetcherResult.<List<Show>>newResult().data(shows).build();
+        }
     }
 }
