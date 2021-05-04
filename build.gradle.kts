34ad4b5dfc591fb63d8cad86badf15b18459d56e
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2021-05-04 16:10:48 -0700

    Enabling mavenLocal

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@
 
 
 plugins {
-	id("java")
-	id("org.springframework.boot") version "2.4.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("com.netflix.dgs.codegen") version "4.6.4"
+    id("java")
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.netflix.dgs.codegen") version "4.6.4"
 }
 
 group = "com.example"
@@ -27,32 +27,33 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
-	mavenCentral()
+    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
-	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
-	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.github.javafaker:javafaker:1.+")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.github.javafaker:javafaker:1.+")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
-	generateClient = true
-	packageName = "com.example.demo.generated"
+    generateClient = true
+    packageName = "com.example.demo.generated"
 }
 
 tasks.withType<JavaCompile> {
-	java {
-		targetCompatibility = JavaVersion.VERSION_1_8
-		sourceCompatibility = JavaVersion.VERSION_1_8
-	}
+    java {
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
