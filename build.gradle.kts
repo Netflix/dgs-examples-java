c31a43400cc8ee4d4d3f0f915292e41983f5596f
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2021-05-06 19:41:56 -0700

    Remove unnecessary spring-boot-dependency plugin
    We are including the graphql-dgs-platform-dependencies BOM and through
    it manage dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,8 @@
 
 plugins {
     id("java")
-    id("org.springframework.boot") version "2.4.5"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.netflix.dgs.codegen") version "4.6.4"
+    id("org.springframework.boot") version "2.4.5"
 }
 
 group = "com.example"
