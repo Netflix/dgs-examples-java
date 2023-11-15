7d6e16c422dc9590dcbe8b824782ed12f6b24825
ksrinivasan <ksrinivasan@netflix.com>
2023-11-15 15:25:30 -0800

    Fix build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ repositories {
 
 dependencyManagement {
 	imports {
-	mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.1.0-SNAPSHOT")
+	mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.0.1")
 	}
 }
 
@@ -56,7 +56,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.1.0-SNAPSHOT"))
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.0.1"))
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
     implementation("com.netflix.graphql.dgs:graphql-dgs-subscriptions-websockets-autoconfigure")
