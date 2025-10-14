1880361c5c1f9cb1911a869742b0ec05990a4305
Paul Bakker <pbakker@netflix.com>
2025-10-14 19:05:02 -0400

    upgrade codegen

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version "4.0.0-M3"
     id("io.spring.dependency-management") version "1.1.4"
-    id("com.netflix.dgs.codegen") version "6.1.4"
+    id("com.netflix.dgs.codegen") version "8.1.1"
 }
 apply(plugin = "com.netflix.dgs.codegen")
 
